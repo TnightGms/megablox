55e4626015c7751c992d40be97f91b51b2f640d2
TnightGms <celtanews@hotmail.com>
2026-02-21 18:23:56 +0100

    Create forum.js

---
+++ forum.js
@@ -0,0 +1,24 @@
+function loadForum(){
+  const msgs = JSON.parse(localStorage.getItem("megabloxForum")) || [];
+  const div = document.getElementById("messages");
+  div.innerHTML="";
+  msgs.forEach(m=>{
+    let badge = (m.role==="admin"||m.role==="superadmin") ? 
+                `<img src="assets/AdminICON.webp" width="15">` : "";
+    div.innerHTML += `<p><b>${m.user}:</b> ${badge} ${m.text}</p>`;
+  });
+}
+
+function sendMsg(){
+  const user = JSON.parse(localStorage.getItem("megabloxCurrentUser")).username;
+  const role = JSON.parse(localStorage.getItem("megabloxCurrentUser")).role;
+  const text = document.getElementById("msg").value;
+  if(!text) return;
+  const msgs = JSON.parse(localStorage.getItem("megabloxForum")) || [];
+  msgs.push({user, role, text});
+  localStorage.setItem("megabloxForum", JSON.stringify(msgs));
+  document.getElementById("msg").value="";
+  loadForum();
+}
+
+loadForum();
