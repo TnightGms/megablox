81e97378032b99dfa7047ddc1f961e907c9bd3ac
TnightGms <celtanews@hotmail.com>
2026-02-21 18:18:15 +0100

    Delete public directory

--- public/admin.js
+++
@@ -1,8 +0,0 @@
-async function makeAdmin(){
-  await fetch("/api/makeAdmin", {
-    method:"POST",
-    headers:{"Content-Type":"application/json"},
-    body: JSON.stringify({ userId:adminId.value })
-  });
-  alert("Ahora es admin");
-}
